d7f56ebc3be22b4a85efa02e95772781683d0892
Slawek <s28161@pjwstk.edu.pl>
2024-03-15 13:21:08 +0100

    task5, handling the case of end being smaller than start

--- SquareGenerator.py
+++ SquareGenerator.py
@@ -3,6 +3,9 @@ import math
 
 class SquareGenerator:
     def generate_squares(self, start, end):
+
+        if end < start:
+            raise ValueError("End of range cannot be less than start of range.")
         squares = []
         for num in range(start, end + 1):
             squares.append(num ** 2)
