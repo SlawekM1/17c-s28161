6bdc9a0ada96704dd388a72a5ffc46b77885b330
Slawek <s28161@pjwstk.edu.pl>
2024-03-15 13:16:25 +0100

    task4, math calculations

--- SquareGenerator.py
+++ SquareGenerator.py
@@ -7,3 +7,8 @@ class SquareGenerator:
         for num in range(start, end + 1):
             squares.append(num ** 2)
         return squares
+
+
+    def calculate_square_roots(self, numbers):
+        square_roots = [math.sqrt(num) for num in numbers]
+        return square_roots
